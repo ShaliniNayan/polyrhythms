13718a4d3d9e8dabb833c42d90195b6a1fba117d
Shalini Nayan <shalininayan@outlook.com>
2024-12-22 21:23:37 +0530

    Implement the arc to return Position

--- track.js
+++ track.js
@@ -4,6 +4,12 @@ class Track {
 		this.radius = radius;
 	}
 
+	getPostion(offset) {
+		return { x: this.center.x + Math.cos(offset) * this.radius, 
+				y: this.center.y - Math.sin(offset) * this.radius 
+			};
+	}
+
 	draw(ctx) {
 		ctx.beginPath();
 		ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
