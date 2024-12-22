fda81e75b82245ea4e986d5ff54d1cae2a710724
Shalini Nayan <shalininayan@outlook.com>
2024-12-22 21:18:22 +0530

    Add ball file and update the constructor track spped and radius

---
+++ ball.js
@@ -0,0 +1,27 @@
+class Ball {
+	constructor(track, radius, speed) {
+		this.track = track;
+		this.radius = radius;
+		this.speed = speed;
+		this.offset = 0;
+		this.center = this.track.getPostion(this.offset);
+		// this.x = 0;
+		// this.y = 0;
+		// this.dx = 0;
+		// this.dy = 0;
+		// this.updatePosition();
+	}
+	// updatePosition() {
+	// 	this.x = this.track.center.x + this.radius * Math.cos(this.offset);
+	// 	this.y = this.track.center.y + this.radius * Math.sin(this.offset);
+	// 	this.dx = this.speed * Math.cos(this.offset);
+	// 	this.dy = this.speed * Math.sin(this.offset);
+	// 	this.offset += this.speed;
+	// } 
+	draw(ctx) {
+		ctx.beginPath();
+		ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
+		ctx.strokeStyle = "white";
+		ctx.stroke();	
+	}
+}
