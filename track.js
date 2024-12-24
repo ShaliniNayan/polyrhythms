567971a006852052f5b851e3495cedf712e791d8
Shalini Nayan <shalininayan@outlook.com>
2024-12-24 12:57:43 +0530

    Update the ball position and shape

--- track.js
+++ track.js
@@ -5,14 +5,21 @@ class Track {
 	}
 
 	getPostion(offset) {
-		return { x: this.center.x + Math.cos(offset) * this.radius, 
+		return { x: this.center.x + Math.cos(offset * 3) * this.radius, // *3 move the ball according to shape
 				y: this.center.y - Math.sin(offset) * this.radius 
 			};
 	}
 
 	draw(ctx) {
 		ctx.beginPath();
-		ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
+		for (let a = 0; a < Math.PI * 2; a += 0.1) {
+			ctx.lineTo(
+				this.center.x + Math.cos(a * 3) * this.radius, // *3 change the shape
+				this.center.y - Math.sin(a) * this.radius
+			);
+		}
+		ctx.closePath();
+		// ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
 		ctx.strokeStyle = "white";
 		ctx.stroke();
 	}
