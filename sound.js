ccec02f37f771794a4faa61a38b691aa474339ef
Shalini Nayan <shalininayan@outlook.com>
2024-12-24 22:18:13 +0530

    Add the function for parameter of sound

--- sound.js
+++ sound.js
@@ -1,1 +1,9 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+function playSound(frequency = 440, duration = 1) {
+	const oscillator = audioCtx.createOscillator();
+	oscillator.connect(audioCtx.destination); // Connect the oscillator to the audio context destination (output)
+	oscillator.frequency.setValueAtTime(frequency, audioCtx.currentTime); // Set the initial frequency = frequency; // Set the frequency of the oscillator (in Hz)
+	oscillator.start(); // Start the oscillator
+	oscillator.stop(audioCtx.currentTime + duration); // Stop the oscillator after the specified duration
+}
